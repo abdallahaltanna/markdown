3c727df9d81846f09b3c40f086a267bddba37030
abdallahaltanna <abdallahmyaltanna@gmail.com>
2021-09-20 19:46:05 +0300

    add files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+
+function App() {
+  const [markdown, setMarkdown] = useState('## Markdown');
+  return (
+    <main>
+      <section className='markdown'>
+        <textarea
+          className='input'
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+      </section>
+      <article className='result'>
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </article>
+    </main>
+  );
+}
+
+export default App;
